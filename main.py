99eca5dc88a2e4fc37cd511c5e094b0401434fef
Ximen <Cheng.Ximen@bci.ca>
2023-07-14 11:24:20 -0700

    update us small cap specific file path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore")
 
 
 def main():
-    this_qtr = '2023-04-30'
+    this_qtr = '2023-06-30'
     last_qtr = '2023-03-31'
     dashboard = lib.Dashboard(
         this_qtr, last_qtr, config.INTERNAL_JSON_DIR, config.INTERNAL_DIR)
