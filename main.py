caf7cfd60e4141c1fd64b34e8d7b2ed906e1069c
Ximen <Cheng.Ximen@bci.ca>
2023-12-12 17:24:21 -0800

    remove shroders and ca_aq

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ warnings.filterwarnings("ignore")
 
 
 def main():
-    this_qtr = '2023-09-30'
-    last_qtr = '2023-06-30'
+    this_qtr = '2023-11-30'
+    last_qtr = '2023-09-30'
     dashboard = lib.Dashboard(
         this_qtr, last_qtr, config.INTERNAL_JSON_DIR, config.INTERNAL_DIR)
     dashboard.process()
